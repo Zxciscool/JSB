c1741f6978b31989cfbcb54c3a0752ddd6143534
Zxciscool <maksym.golubov@gmail.com>
2024-10-09 15:53:15 +0300

    add new home work

---
+++ lection 3/task1.js
@@ -0,0 +1,30 @@
+/*Вам необхідно написати програму, яка приймає на вхід число і виводить
+ у консоль повідомлення залежно від значення числа. Якщо число ділиться на 3,
+  то повідомлення має бути Fizz, якщо число ділиться на 5, то повідомлення має бути Buzz, 
+  а якщо число ділиться і на 3, і на 5, то повідомлення має бути FizzBuzz. Наприклад,
+   для числа 15 повідомлення має бути FizzBuzz.*/
+
+   const userNumber = +prompt('Please enter number');
+
+
+    if ( userNumber % 5 !== 0 && userNumber % 3 === 0 ) {
+
+    console.log('Fizz');
+
+   }
+
+    else if (userNumber % 3 !== 0 && userNumber % 5 === 0) {
+
+    console.log('Buzz');
+
+   } 
+
+   else  if (userNumber % 3 === 0 && userNumber % 5 === 0) {
+
+    console.log('FizzBuzz');
+
+   }
+
+   else {
+    console.log ('wrong number');
+   }
